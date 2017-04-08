8116a587bfc0ca5dfbc340b5e1dd44766bec96ba
zhen.yu <iszhenyu@yeah.net>
2017-04-08 17:38:34 +0800

    ..

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: xiaoz
+@time: 2017/4/8 下午5:34
+"""
+from numpy import *
+
+
+def func():
+    print random.rand(4, 4)
+
+
+if __name__ == '__main__':
+    func()
