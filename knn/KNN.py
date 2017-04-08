eded0f431144298b48bfb0cd9c214fc19034c965
zhen.yu <iszhenyu@yeah.net>
2017-04-08 23:21:42 +0800

    ..

--- knn/KNN.py
+++ knn/KNN.py
@@ -3,6 +3,7 @@
 @author: xiaoz
 @time: 2017/4/8 下午10:49
 """
+from __future__ import unicode_literals
 from numpy import *
 import operator
 
@@ -18,7 +19,22 @@ def create_data_set():
     return group, labels
 
 
+def classify(in_x, group, labels, k):
+    data_set_size = group.shape[0]
+    diff_mat = tile(in_x, (data_set_size, 1)) - group
+    sq_diff_mat = diff_mat ** 2
+    sq_distances = sq_diff_mat.sum(axis=1)
+    distances = sq_distances ** 0.5
+    sorted_distances = distances.argsort()
+
+    class_count = {}
+    for i in range(k):
+        vote_label = labels[sorted_distances[i]]
+        class_count[vote_label] = class_count.get(vote_label, 0) + 1
+    sorted_class_count = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
+
+
 if __name__ == '__main__':
     group, labels = create_data_set()
-    print group
-    print labels
+    print classify([0, 0], group, labels, 3)
