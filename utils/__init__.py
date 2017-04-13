38607abddcc9feccb06d033e78b36ec70348092f
zhen.yu <iszhenyu@yeah.net>
2017-04-13 17:48:30 +0800

    ..

---
+++ utils/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: xiaoz
+@time: 2017/4/13 下午5:43
+"""
+from __future__ import unicode_literals
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
