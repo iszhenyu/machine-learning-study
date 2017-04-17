09294b94f09f2fbc2db5047ead99838122d64b48
zhen.yu <iszhenyu@yeah.net>
2017-04-17 22:52:00 +0800

    ..

--- algorithms/trees.py
+++ algorithms/trees.py
@@ -7,6 +7,8 @@ from __future__ import unicode_literals
 
 from math import log
 
+import operator
+
 
 def cal_shannon_entropy(data_set):
     """
@@ -70,3 +72,41 @@ def choose_best_feature_to_split(data_set):
             biggest_entropy_difference = entropy_difference
             best_feature = i
     return best_feature
+
+
+def majority_cnt(class_list):
+    class_count = {}
+    for vote in class_list:
+        if vote not in class_count:
+            class_count[vote] = 0
+        class_count[vote] += 1
+    sorted_class_count = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
+
+
+def create_tree(data_set, labels):
+    """
+    创建树
+    :param data_set:
+    :param labels:
+    :return:
+    """
+    class_list = [item[-1] for item in data_set]
+    # 类别完全相同则停止继续划分
+    if class_list.count(class_list[0]) == len(class_list):
+        return class_list[0]
+    # 遍历完所有特征, 类标签仍然不唯一, 返回出现次数最多的
+    if len(data_set[0]) == 1:
+        return majority_cnt(class_list)
+
+    # 递归建树
+    best_feature = choose_best_feature_to_split(data_set)
+    best_label = labels[best_feature]
+    tree = {best_label: {}}
+    del labels[best_feature]
+    feature_values = [item[best_feature] for item in data_set]
+    unique_feature_values = set(feature_values)
+    for val in unique_feature_values:
+        sub_labels = labels[:]
+        tree[best_label][val] = create_tree(split_data_set(data_set, best_feature, val), sub_labels)
+    return tree
