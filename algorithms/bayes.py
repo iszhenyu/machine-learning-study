a80484ce6d6405b5368ccdb8021665bc124f9e2f
zhen.yu <iszhenyu@yeah.net>
2017-04-19 23:12:22 +0800

    ..

--- algorithms/bayes.py
+++ algorithms/bayes.py
@@ -6,5 +6,18 @@
 from __future__ import unicode_literals
 
 
-if __name__ == '__main__':
-    pass
+def create_vocab_list(data_set):
+    vocab_set = set([])
+    for item in data_set:
+        vocab_set = vocab_set | set(item)
+    return list(vocab_set)
+
+
+def words_2_vec(vocab_list, input_data):
+    return_vec = [0] * len(vocab_list)
+    for word in input_data:
+        if word in vocab_list:
+            return_vec[vocab_list.index(word)] = 1
+        else:
+            print 'the word: %s is not in Vocabulary' % word
+    return return_vec
---
+++ demos/bayes_demo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@author: xiaoz
+@time: 2017/4/18 下午7:31
+"""
+from __future__ import unicode_literals
+
+
+def create_data():
+    posting_list = [
+        ['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+        ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+        ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+        ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+        ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+        ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']
+    ]
+    class_vec = [0, 1, 0, 1, 0, 1]
+    return posting_list, class_vec
+
+
+if __name__ == '__main__':
+    pass
